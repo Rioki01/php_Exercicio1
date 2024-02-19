636418975b313d07bc3a0f67fc87443ca3a5ab04
Rioki01 <134452025+Rioki01@users.noreply.github.com>
2024-02-19 16:44:34 -0300

    Atualizar o exercicios2.php

--- exercicios2.php
+++ exercicios2.php
@@ -80,8 +80,94 @@ $palavraseparadas = explode(" ",$fraseexercicio5);
 print_r($palavraseparadas);
 
     ////////******************************************///
-    /**6**/
-    echo "<br>6)<br>";
-    echo "<br>";
+/*********6**********/
+echo "<br>6)";
+echo "<br>Crie uma Função que verifica se a palavra é palindromo.";
+echo "<br>";
+function palindromo($frase){
+if (strrev($frase) == $frase){
+echo "A frase é um palindromo!";
+}
+else{
+echo "A frase não é um palindromo!";
+}
+}
+$frasepalindromoex6 = "abba";
+palindromo($frasepalindromoex6);
+/*********7**********/
+echo "<br>7)";
+echo "<br>Crie um programa que imprima numeros de 1 a 20, substituindo 
+multiplos de 3 por 'fizz' e de 5 por 'buzz'.";
+echo "<br>";
+function ex7(){
+for($i = 1; $i <= 20; $i++)
+{
+if($i % 3 == 0)
+{
+echo "fizz,";
+}else if ($i % 5 == 0)
+{
+echo "buzz,";
+}
+else
+{
+echo "$i,";
+}
+}
+}
+ex7();
+/*********8**********/
+echo "<br>8)";
+echo "<br>Crie uma função que remova espaços em branco de uma string";
+echo "<br>";
+function ex8($ex8frases){
+echo str_replace(" ", "", $ex8frases);
+}
 
+$ex8frase = "frase com varios espaços!!";
+ex8($ex8frase);
+/*********9**********/
+echo "<br>9)";
+echo "<br>Crie um programa que imprima os numeros fibbonaci até o decimo 
+termo.";
+echo "<br>";
+function ex9fibbonaci()
+{
+$numero = 0;
+$num1 = 0;
+$num2 = 1;
+echo $num1.' '.$num2.' ';
+while($numero < 10)
+{
+$num3 = $num1 + $num2;
+echo $num3.' ';
+$num1 = $num2;
+$num2 = $num3;
+$numero = $numero + 1;
+}
+} 
+ex9fibbonaci();
+/*********10**********/
+echo "<br>";
+echo "<br>10)";
+echo "<br>Crie uma funcao que receba um texto e retorna se é um 
+pangrama(contem todas letras do alfabeto).";
+function ex10($ex10){
+$frase = strtolower(trim($ex10));
+$letras = str_split("thequickbrownfoxjumpsoverthelazydog");
+foreach($letras as $letra){
+if(!strstr($frase, $letra))
+{
+return 0;
+}
+}
+return 1;
+}
+$fraseex10 = "the quick brown fox jumps over the lazy dog";
+
+if(ex10($fraseex10)){
+echo "<br> É um pangrama!";
+}else{
+echo "<br>NÃO é um pangrama!";
+}
 ?>
